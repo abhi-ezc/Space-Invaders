e7bf1c9128291a7e6587621a7ba22950f05b8e31
Abhinav MK <abhinav.mk@gravityilabs.com>
2024-07-22 23:38:49 +0530

    Feature-1-SFML: draw some shapes (circle, square & triangle)

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -18,7 +18,36 @@ int main()
         }
 
         // clear the window and filling it with blue color
-        window.clear(sf::Color::Blue);
+        window.clear(sf::Color::Black);
+
+        // creating circle
+
+        sf::CircleShape circle;
+        circle.setRadius(100); // setting circle raidus to 100
+        circle.setPosition(( window.getSize().x / 2 ) - circle.getRadius(), ( window.getSize().y / 2 ) - circle.getRadius()); // setting the position of circle to center of the screen
+        circle.setFillColor(sf::Color::Green); // filling circle with green color
+
+        // creating square
+        sf::RectangleShape square=sf::RectangleShape(sf::Vector2f(60, 60)); 
+        square.setPosition(500, 200); // setting position of square
+        square.setFillColor(sf::Color::Red); // filling square with red color
+
+        // creating triangle
+        sf::ConvexShape triangle = sf::ConvexShape(3); // creating triangle  using convex shape with 3 points
+        triangle.setPosition(500, 500); // setting triangle position
+        triangle.setPoint(0, sf::Vector2f(0, 0)); // setting the position of first point
+        triangle.setPoint(1, sf::Vector2f(-60, 100)); // setting the position of second point
+        triangle.setPoint(2, sf::Vector2f(60, 100));  // setting the position of third point
+        triangle.setFillColor(sf::Color::Blue); // filling the triangle with blue color
+
+        // draw circle
+        window.draw(circle);
+
+        // draw square
+        window.draw(square);
+
+        //draw triangle
+        window.draw(triangle);
 
         // display whatever you draw
         window.display();
