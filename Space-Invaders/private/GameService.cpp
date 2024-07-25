39285e130d1e4a2c6e0315bfcd73b7e013c9d416
Abhinav MK <abhinav.mk@gravityilabs.com>
2024-07-25 20:25:39 +0530

    Feature-2-OOP: Add GameService class for handling game loop

---
+++ Space-Invaders/private/GameService.cpp
@@ -0,0 +1,33 @@
+#include "../public/GameService.h"
+
+void GameService::initialize() {
+	
+}
+
+void GameService::destroy() {
+
+}
+
+GameService::GameService() {
+	
+}
+
+GameService::~GameService() {
+
+}
+
+void GameService::ignite() {
+
+}
+
+void GameService::update() {
+
+}
+
+void GameService::render() {
+
+}
+
+bool GameService::isRunning() {
+	return false;
+}
