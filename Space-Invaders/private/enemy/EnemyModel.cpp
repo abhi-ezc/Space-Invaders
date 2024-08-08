1c8e855ebed988ae2545d43e644f428769d25ac0
Abhinav MK <abhinav.mk@gravityilabs.com>
2024-08-08 21:16:35 +0530

    Feature-5-Enemies: implement enemy movement

--- Space-Invaders/private/enemy/EnemyModel.cpp
+++ Space-Invaders/private/enemy/EnemyModel.cpp
@@ -11,7 +11,27 @@ namespace Enemy
 
     void EnemyModel::render() { }
 
-    sf::Vector2f EnemyModel::getPlayerPosition() { return m_current_position; }
-
+    sf::Vector2f EnemyModel::getCurrentPosition() { return m_current_position; }
     void EnemyModel::setCurrentPosition(sf::Vector2f current_position) { m_current_position = current_position; }
+
+    MovementDirection EnemyModel::getMovementDirection() { return m_movement_direction; }
+
+    void EnemyModel::setMovementDirection(MovementDirection movementDirection)
+    {
+        m_movement_direction = movementDirection;
+    }
+
+    MovementDirection EnemyModel::getPreviousDirection() { return m_previous_direction; }
+    void EnemyModel::setPreviousDirection(MovementDirection direction) { m_previous_direction = direction; }
+
+    float EnemyModel::getVerticalTravelDistance() { return m_vertical_travel_distance; }
+
+    int EnemyModel::getMovementSpeed() { return m_movement_speed; }
+
+    sf::Vector2f EnemyModel::getLeftMostPosition() { return m_left_most_position; }
+    sf::Vector2f EnemyModel::getRightMostPosition() { return m_right_most_position; }
+    float EnemyModel::getDownMostPositionY() { return m_ref_position.y + m_vertical_travel_distance; }
+
+    sf::Vector2f EnemyModel::getReferencePosition() { return m_ref_position; }
+    void EnemyModel::setReferencePosition(sf::Vector2f refPosition) { m_ref_position = refPosition; }
 }
--- Space-Invaders/public/enemy/EnemyModel.h
+++ Space-Invaders/public/enemy/EnemyModel.h
@@ -3,11 +3,29 @@
 
 namespace Enemy
 {
+    enum class MovementDirection
+    {
+        LEFT,
+        DOWN,
+        RIGHT
+    };
+
     class EnemyModel
     {
         private:
             sf::Vector2f m_spawn_position = sf::Vector2f(0, 0);
             sf::Vector2f m_current_position;
+            sf::Vector2f m_ref_position;
+
+            MovementDirection m_movement_direction = MovementDirection::RIGHT;
+            MovementDirection m_previous_direction = MovementDirection::RIGHT;
+
+            int m_movement_speed = 300.f;
+            const float m_vertical_travel_distance = 30.f;
+
+            const sf::Vector2f m_left_most_position = sf::Vector2f(50.f, 950.f);
+            const sf::Vector2f m_right_most_position = sf::Vector2f(1800.f, 950.f);
+
         public:
             EnemyModel();
             ~EnemyModel();
@@ -16,8 +34,23 @@ namespace Enemy
             void update();
             void render();
 
-            sf::Vector2f getPlayerPosition();
-
+            sf::Vector2f getCurrentPosition();
             void setCurrentPosition(sf::Vector2f current_position);
+
+            MovementDirection getMovementDirection();
+            void setMovementDirection(MovementDirection movementDirection);
+            MovementDirection getPreviousDirection();
+            void setPreviousDirection(MovementDirection direction);
+
+            float getVerticalTravelDistance();
+
+            int getMovementSpeed();
+
+            sf::Vector2f getLeftMostPosition();
+            sf::Vector2f getRightMostPosition();
+            float getDownMostPositionY();
+
+            sf::Vector2f getReferencePosition();
+            void setReferencePosition(sf::Vector2f refPosition);
     };
 }
